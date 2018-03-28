45d5fff2a6a722f4bd69500311461fb00c802541
iceberg20 <italoberg@yahoo.com.br>
2018-03-28 11:39:17 -0300

    Citzen create - adiciona um citzen ao  banco de dado

--- app/controllers/api/v1/citzens_controller.rb
+++ app/controllers/api/v1/citzens_controller.rb
@@ -9,6 +9,20 @@ module Api
 			def show
 				citzen = Citzen.find(params[:id])
 				render json: {status: 'SUCCESS', message:'Loaded citzen', data:citzen},status: :ok
+			end
+
+			def create
+				citzen = Citzen.new(citzen_params)
+				if citzen.save
+					render json: {status: 'SUCCESS', message:'Saved citzen', data:citzen},status: :ok
+				else
+					render json: {status: 'ERROR', message:'citzen not saved', data:citzen.erros},status: :unprocessable_entity
+				end
+			end
+
+			private
+			def citzen_params
+				params.permit(:name, :age, :gender)
 			end   
 		end
 	end
