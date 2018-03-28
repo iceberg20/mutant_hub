6701b926c393d99052d18f391ecd1737dd3a34e0
iceberg20 <italoberg@yahoo.com.br>
2018-03-28 10:44:12 -0300

    Citzen show - exibe citzen pelo id

--- app/controllers/api/v1/citzens_controller.rb
+++ app/controllers/api/v1/citzens_controller.rb
@@ -4,7 +4,12 @@ module Api
 			def index
 				citzens = Citzen.order('created_at DESC');
 				render json: {status: 'SUCCESS', message:'Citzens loaded!', data:citzens},status: :ok
-			end 
+			end
+
+			def show
+				citzen = Citzen.find(params[:id])
+				render json: {status: 'SUCCESS', message:'Loaded citzen', data:citzen},status: :ok
+			end   
 		end
 	end
 end
