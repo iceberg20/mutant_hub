c75ed8c8765be20cdc7a9686cc2a45efa1ef054b
iceberg20 <italoberg@yahoo.com.br>
2018-03-30 12:07:45 -0300

    Cria método - informa se Citzen está infectado

--- app/models/citzen.rb
+++ app/models/citzen.rb
@@ -2,4 +2,37 @@ class Citzen < ApplicationRecord
 	validates :name, presence: true
 	validates :age, presence: true
 	validates :gender, presence: true
+
+	def neon_infected?
+		if self.neon_flag.nil?
+			return false
+		else
+			if self.neon_flag>=3
+				return true
+			else
+				return false
+			end
+		end
+	end
+
+	def eye_infected?
+		if self.eye_flag.nil?
+			return false
+		else
+			if self.eye_flag>=3
+				return true
+			else
+				return false
+			end
+		end
+	end
+
+	def infected?
+		if eye_infected? or neon_infected?
+			return true
+		else
+			return false
+		end
+	end
+
 end
